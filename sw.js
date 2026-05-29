07aefec6e55a3d83a95a7db360c7b36b496b81e5
Erick Mulenga <mulengaerick9@gmail.com>
2026-05-29 05:02:32 +0000

    Add PWA support - installable as app on phone and desktop

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ehp-cache-v1';
+const urlsToCache = [
+  '/mbabala-ehp/',
+  '/mbabala-ehp/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        var responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+});
